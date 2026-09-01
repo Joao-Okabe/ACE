faf216d62abde11e059e1fb71fb78a614b2de1c8
Tonhoka <joao.okabe.berghem@gmail.com>
2026-09-01 15:45:30 -0300

    RF03 e RF04

--- database/script.sql
+++ database/script.sql
@@ -82,3 +82,196 @@ CREATE TABLE documentos_aluno (
         REFERENCES aluno(cd_aluno)
         ON DELETE CASCADE,
 );
+
+
+CREATE TABLE competicao (
+    cd_competicao INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nm_competicao VARCHAR(150) NOT NULL, 
+    criado em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    dt_encerramento TIMESTAMP,
+)
+
+CREATE TABLE formato (
+    cd_formato INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nm_formato VARCHAR(30) NOT NULL,
+    ds_formato VARCHAR(2048) NOT NULL,
+)
+
+CREATE TABLE modalidade (
+    cd_modalidade INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ds_restr_genero CHAR(5) NOT NULL
+    ds_restr_idade CHAR(5) NOT NULL
+)
+
+CREATE TABLE esporte (
+    cd_esporte INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nm_esporte VARCHAR(30) NOT NULL,
+    ds_esporte VARCHAR(2048) NOT NULL,
+)
+
+CREATE TABLE tipo etapa (
+    cd_tipo_etapa INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cd_esporte INTEGER NOT NULL,
+    nm_tipo_etapa VARCHAR(50) NOT NULL,
+    ds_tipo_etapa VARCHAR(255) NOT NULL,
+    ordem INTEGER,
+    CONSTRAINT fk_esporte
+        FOREIGN KEY (cd_esporte)
+        REFERENCES esporte(cd_esporte)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE time (
+    cd_time INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nm_time VARCHAR(50) NOT NULL UNIQUE,
+    path brasão VARCHAR(255) NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    atualizado TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+)
+
+CREATE TABLE vinculo_time_escola (
+    cd_escola INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    CONSTRAINT fk_escola
+        FOREIGN KEY (cd_escola)
+        REFERENCES escola(cd_escola)
+        ON DELETE CASCADE 
+)
+
+CREATE TABLE vinculo_time_integrante (
+    cd_usuario INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    cd_funcao_integrante INTEGER NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    CONSTRAINT fk_usuario 
+        FOREIGN KEY (cd_usuario)
+        REFERENCES usuario(cd_usuario)
+        ON DELETE CASCADE
+    CONSTRAINT fk_time 
+        FOREIGN KEY (cd_time)
+        REFERENCES time(cd_time)
+        ON DELETE CASCADE
+    CONSTRAINT fk_funcao_integrante 
+        FOREIGN KEY (cd_funcao_integrante )
+        REFERENCES usuario(cd_funcao_integrante )
+        ON DELETE CASCADE
+)
+
+CREATE TABLE responsavel (
+    cd_responsavel INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cd_usuario INTEGER NOT NULL UNIQUE,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_usuario
+        FOREIGN KEY (cd_usuario)
+        REFERENCES usuario(cd_usuario)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE vinculo_time_responsavel (
+    cd_responsavel INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    CONSTRAINT fk_time
+        FOREIGN KEY (cd_time)
+        REFERENCES time(cd_time)
+        ON DELETE CASCADE
+    CONSTRAINT fk_usuario
+        FOREIGN KEY (cd_usuario)
+        REFERENCES usuario(cd_usuario)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE funcao_integrate (
+    cd_funcao_integrante INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cd_esporte INTEGER NOT NULL,
+    nm_funcao VARCHAR(150) NOT NULL,
+    ds_funcao VARCHAR(1024) NOT NULL,
+    CONSTRAINT fk_esporte
+        FOREIGN KEY (cd_esporte)
+        REFERENCES esporte(cd_esporte)
+        ON DELETE CASCADE 
+)
+
+CREATE TABLE partida (
+    cd_partida INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cd_competicao INTEGER NOT NULL UNIQUE,
+    cd_esporte INTEGER NOT NULL UNIQUE,
+    cd_modalidade INTEGER NOT NULL UNIQUE,
+    cd_formato INTEGER NOT NULL UNIQUE,
+    CONSTRAINT fk_competicao
+        FOREIGN KEY (cd_competicao)
+        REFERENCES competicao(cd_competicao)
+        ON DELETE CASCADE
+    CONSTRAINT fk_esporte
+        FOREIGN KEY (cd_esporte)
+        REFERENCES esporte(cd_esporte)
+        ON DELETE CASCADE
+    CONSTRAINT fk_modalideda
+        FOREIGN KEY (cd_modalidade)
+        REFERENCES modalidade(cd_modalidade)
+        ON DELETE CASCADE
+    CONSTRAINT fk_formato
+        FOREIGN KEY (cd_formato)
+        REFERENCES formato(cd_formato)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE info_partida (
+    cd_partida INTEGER NOT NULL UNIQUE,
+    dt_partida TIMESTAMP NOT NULL,
+    local_partida VARCHAR(1024) NOT NULL,
+    CONSTRAINT fk_partida
+        FOREIGN KEY (cd_partida)
+        REFERENCES partida(cd_partida)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE etapa_partida (
+    cd_etapa_partida INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cd_partida INTEGER NOT NULL,
+    cd_tipo_etapa INTEGER NOT NULL,
+    nr_etapa INTEGER NOT NULL,
+    inicio_em TIMESTAMP NOT NULL,
+    fim_em TIMESTAMP NOT NULL,
+    CONSTRAINT fk_partida
+        FOREIGN KEY (cd_partida)
+        REFERENCES partida(cd_partida)
+        ON DELETE CASCADE
+    CONSTRAINT fk_tipo_etapa
+        FOREIGN KEY (cd_tipo_etapa)
+        REFERENCES tipo etapa(cd_tipo_etapa)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE resultado_etapa_partida (
+    cd_etapa_partida INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    pontuação INTEGER NOT NULL,
+    CONSTRAINT fk_etapa_partida 
+        FOREIGN KEY (cd_etapa_partida )
+        REFERENCES etapa_partida (cd_etapa_partida )
+        ON DELETE CASCADE
+    CONSTRAINT fk_time
+        FOREIGN KEY (cd_time)
+        REFERENCES time(cd_time)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE vinculo_partida_time (
+    cd_partida INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    CONSTRAINT fk_partida 
+        FOREIGN KEY (cd_partida)
+        REFERENCES partida(cd_partida)
+        ON DELETE CASCADE
+    CONSTRAINT fk_time 
+        FOREIGN KEY (cd_time)
+        REFERENCES time(cd_time)
+        ON DELETE CASCADE
+)
