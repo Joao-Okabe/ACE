884f734da649187ffaf53c9f36034bee98396be9
Joao-Okabe <joao.okae.berghem@gmail.com>
2026-09-03 16:50:27 -0300

    Merge Etec-Tonho

--- database/script.sql
+++ database/script.sql
@@ -80,7 +80,7 @@ CREATE TABLE documentos_aluno (
     CONSTRAINT fk_cd_aluno
         FOREIGN KEY (cd_aluno)
         REFERENCES aluno(cd_aluno)
-        ON DELETE CASCADE,
+        ON DELETE CASCADE
 );
 
 CREATE TABLE competicao (
@@ -99,22 +99,22 @@ CREATE TABLE competicao (
 CREATE TABLE formato (
     cd_formato INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nm_formato VARCHAR(30) NOT NULL,
-    ds_formato VARCHAR(2048) NOT NULL,
-)
+    ds_formato VARCHAR(2048) NOT NULL
+);
 
 CREATE TABLE modalidade (
     cd_modalidade INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    ds_restr_genero CHAR(5) NOT NULL
+    ds_restr_genero CHAR(5) NOT NULL,
     ds_restr_idade CHAR(5) NOT NULL
-)
+);
 
 CREATE TABLE esporte (
     cd_esporte INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nm_esporte VARCHAR(30) NOT NULL,
-    ds_esporte VARCHAR(2048) NOT NULL,
-)
+    ds_esporte VARCHAR(2048) NOT NULL
+);
 
-CREATE TABLE tipo etapa (
+CREATE TABLE tipo_etapa (
     cd_tipo_etapa INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cd_esporte INTEGER NOT NULL,
     nm_tipo_etapa VARCHAR(50) NOT NULL,
@@ -124,73 +124,16 @@ CREATE TABLE tipo etapa (
         FOREIGN KEY (cd_esporte)
         REFERENCES esporte(cd_esporte)
         ON DELETE CASCADE
-)
+);
 
 CREATE TABLE time (
     cd_time INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nm_time VARCHAR(50) NOT NULL UNIQUE,
     path brasão VARCHAR(255) NOT NULL,
     ativo BOOLEAN DEFAULT TRUE NOT NULL,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    atualizado TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-)
-
-CREATE TABLE vinculo_time_escola (
-    cd_escola INTEGER NOT NULL,
-    cd_time INTEGER NOT NULL,
-    ativo BOOLEAN DEFAULT TRUE,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    CONSTRAINT fk_escola
-        FOREIGN KEY (cd_escola)
-        REFERENCES escola(cd_escola)
-        ON DELETE CASCADE 
-)
-
-CREATE TABLE vinculo_time_integrante (
-    cd_usuario INTEGER NOT NULL,
-    cd_time INTEGER NOT NULL,
-    cd_funcao_integrante INTEGER NOT NULL,
-    ativo BOOLEAN DEFAULT TRUE,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    CONSTRAINT fk_usuario 
-        FOREIGN KEY (cd_usuario)
-        REFERENCES usuario(cd_usuario)
-        ON DELETE CASCADE
-    CONSTRAINT fk_time 
-        FOREIGN KEY (cd_time)
-        REFERENCES time(cd_time)
-        ON DELETE CASCADE
-    CONSTRAINT fk_funcao_integrante 
-        FOREIGN KEY (cd_funcao_integrante )
-        REFERENCES usuario(cd_funcao_integrante )
-        ON DELETE CASCADE
-)
-
-CREATE TABLE responsavel (
-    cd_responsavel INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    cd_usuario INTEGER NOT NULL UNIQUE,
-    ativo BOOLEAN DEFAULT TRUE,
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_usuario
-        FOREIGN KEY (cd_usuario)
-        REFERENCES usuario(cd_usuario)
-        ON DELETE CASCADE
-)
-
-CREATE TABLE vinculo_time_responsavel (
-    cd_responsavel INTEGER NOT NULL,
-    cd_time INTEGER NOT NULL,
-    ativo BOOLEAN DEFAULT TRUE,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-    CONSTRAINT fk_time
-        FOREIGN KEY (cd_time)
-        REFERENCES time(cd_time)
-        ON DELETE CASCADE
-    CONSTRAINT fk_usuario
-        FOREIGN KEY (cd_usuario)
-        REFERENCES usuario(cd_usuario)
-        ON DELETE CASCADE
-)
+    atualizado TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);
 
 CREATE TABLE funcao_integrate (
     cd_funcao_integrante INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -201,7 +144,64 @@ CREATE TABLE funcao_integrate (
         FOREIGN KEY (cd_esporte)
         REFERENCES esporte(cd_esporte)
         ON DELETE CASCADE 
-)
+);
+
+CREATE TABLE vinculo_time_escola (
+    cd_escola INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_escola
+        FOREIGN KEY (cd_escola)
+        REFERENCES escola(cd_escola)
+        ON DELETE CASCADE 
+);
+
+CREATE TABLE vinculo_time_integrante (
+    cd_usuario INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    cd_funcao_integrante INTEGER NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_usuario 
+        FOREIGN KEY (cd_usuario)
+        REFERENCES usuario(cd_usuario)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_time 
+        FOREIGN KEY (cd_time)
+        REFERENCES time(cd_time)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_funcao_integrante 
+        FOREIGN KEY (cd_funcao_integrante)
+        REFERENCES funcao_integrante(cd_funcao_integrante)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE responsavel (
+    cd_responsavel INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cd_usuario INTEGER NOT NULL UNIQUE,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_usuario
+        FOREIGN KEY (cd_usuario)
+        REFERENCES usuario(cd_usuario)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE vinculo_time_responsavel (
+    cd_responsavel INTEGER NOT NULL,
+    cd_time INTEGER NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    CONSTRAINT fk_time
+        FOREIGN KEY (cd_time)
+        REFERENCES time(cd_time)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_responsavel
+        FOREIGN KEY (cd_responsavel)
+        REFERENCES responsavel(cd_responsavel)
+        ON DELETE CASCADE
+);
 
 CREATE TABLE partida (
     cd_partida INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -212,20 +212,20 @@ CREATE TABLE partida (
     CONSTRAINT fk_competicao
         FOREIGN KEY (cd_competicao)
         REFERENCES competicao(cd_competicao)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT fk_esporte
         FOREIGN KEY (cd_esporte)
         REFERENCES esporte(cd_esporte)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT fk_modalideda
         FOREIGN KEY (cd_modalidade)
         REFERENCES modalidade(cd_modalidade)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT fk_formato
         FOREIGN KEY (cd_formato)
         REFERENCES formato(cd_formato)
         ON DELETE CASCADE
-)
+);
 
 CREATE TABLE info_partida (
     cd_partida INTEGER NOT NULL UNIQUE,
@@ -235,7 +235,7 @@ CREATE TABLE info_partida (
         FOREIGN KEY (cd_partida)
         REFERENCES partida(cd_partida)
         ON DELETE CASCADE
-)
+);
 
 CREATE TABLE etapa_partida (
     cd_etapa_partida INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -247,12 +247,12 @@ CREATE TABLE etapa_partida (
     CONSTRAINT fk_partida
         FOREIGN KEY (cd_partida)
         REFERENCES partida(cd_partida)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT fk_tipo_etapa
         FOREIGN KEY (cd_tipo_etapa)
-        REFERENCES tipo etapa(cd_tipo_etapa)
+        REFERENCES tipo_etapa(cd_tipo_etapa)
         ON DELETE CASCADE
-)
+);
 
 CREATE TABLE resultado_etapa_partida (
     cd_etapa_partida INTEGER NOT NULL,
@@ -261,12 +261,12 @@ CREATE TABLE resultado_etapa_partida (
     CONSTRAINT fk_etapa_partida 
         FOREIGN KEY (cd_etapa_partida )
         REFERENCES etapa_partida (cd_etapa_partida )
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT fk_time
         FOREIGN KEY (cd_time)
         REFERENCES time(cd_time)
         ON DELETE CASCADE
-)
+);
 
 CREATE TABLE vinculo_partida_time (
     cd_partida INTEGER NOT NULL,
@@ -274,9 +274,9 @@ CREATE TABLE vinculo_partida_time (
     CONSTRAINT fk_partida 
         FOREIGN KEY (cd_partida)
         REFERENCES partida(cd_partida)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT fk_time 
         FOREIGN KEY (cd_time)
         REFERENCES time(cd_time)
         ON DELETE CASCADE
-)
+);
--- database/triggers.sql
+++ database/triggers.sql
@@ -1,3 +1,4 @@
+/*Cria função para atualizar a coluna "atualizado_em" de qualquer tabela  */
 CREATE OR REPLACE FUNCTION atualizar_timestamp()
 RETURNS TRIGGER AS
 $$
@@ -7,6 +8,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+/* Chama a função de atualizar antes de alterar as tabelas */
 CREATE TRIGGER trg_usuario_timestamp
 BEFORE UPDATE ON usuario
 FOR EACH ROW
@@ -14,5 +16,10 @@ EXECUTE FUNCTION atualizar_timestamp();
 
 CREATE TRIGGER trg_aluno_timestamp
 BEFORE UPDATE ON aluno
+FOR EACH ROW
+EXECUTE FUNCTION atualizar_timestamp();
+
+CREATE TRIGGER trg_time_timestamp
+BEFORE UPDATE ON time
 FOR EACH ROW
 EXECUTE FUNCTION atualizar_timestamp();
